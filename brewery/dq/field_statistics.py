18f24e21f4bda776ba13ce5947d6e7a54896686f
Stefan Urbanek <stefan.urbanek@gmail.com>
2011-01-12 11:43:03 +0100

    added documentation

--- brewery/dq/field_statistics.py
+++ brewery/dq/field_statistics.py
@@ -10,19 +10,39 @@ except:
 class FieldStatistics(object):
     """Data quality statistics for a dataset field
     
-    Attributes:
-        * field: name of a field which statistics are being presented
-        * record_count: total count of records in dataset
-        * value_count: number of records in which the field exist. In RDB table this is equal to record_count, in
-            document based databse, such as MongoDB it is number of documents that have a key present (being null
-            or not)
-        * value_ratio: ratio of value count to record count, 1 for relational databases
-        * null_count: number of records where field is null
-        * null_value_ratio: ratio of records
-        * unique_storage_type: if there is only one storage type, then this is set to that type
-        * distinct_threshold: number of distict values to collect, if distinct values is greather than threshold,
-            no more values are being collected and distinct_overflow will be set. Set to 0 to get all values.
-            Default is 10.
+    :Attributes:
+        * `field`: name of a field for which statistics are being collected
+
+        * `value_count`: number of records in which the field exist. In relationad database table this
+          is equal to number of rows, in document based databse, such as MongoDB, it is number of
+          documents that have a key present (being null or not)
+
+        * `record_count`: total count of records in dataset. This should be set explicitly on
+          finalisation. Seet :meth:`FieldStatistics.finalize`. In relational database this should be the
+          same as `value_count`.
+
+        * `value_ratio`: ratio of value count to record count, 1 for relational databases
+
+        * `null_count`: number of records where field is null
+
+        * `null_value_ratio`: ratio of records with nulls to total number of probed values =
+          `null_value_ratio` / `value_count`
+
+        * `null_record_ratio`: ratio of records with nulls to total number of records =
+          `null_value_ratio` / `record_count`
+
+        * `empty_string_count`: number of empty strings
+
+        * `storage_types`: list of all encountered storage types (CSV, MongoDB, XLS might have different
+          types within a field)
+
+        * `unique_storage_type`: if there is only one storage type, then this is set to that type
+
+        * `distict_values`: list of collected distinct values
+
+        * `distinct_threshold`: number of distict values to collect, if count of distinct values is
+          greather than threshold, collection is stopped and `distinct_overflow` will be set. Set to 0
+          to get all values. Default is 10.
     """    
     def __init__(self, key, distinct_threshold = 10):
         self.field = key
@@ -44,6 +64,18 @@ class FieldStatistics(object):
         self.unique_storage_type = None
         
     def probe(self, value):
+        """Probe the value:
+        
+        * increase found value count
+        * identify storage type
+        * probe for null and for empty string
+
+        * probe distinct values: if their count is less than ``distinct_threshold``. If there are more
+          distinct values than the ``distinct_threshold``, then distinct_overflow flag is set and list
+          of distinct values will be empty
+                    
+        """
+        
         storage_type = value.__class__
         self.storage_types.add(storage_type.__name__)
 
@@ -54,12 +86,10 @@ class FieldStatistics(object):
         if value == '':
             self.empty_string_count += 1
 
-        self.probe_distinct(value)
+        self._probe_distinct(value)
 
-    def probe_distinct(self, value):
-        """Find distinct values, if theyr count is less than ``distinct_threshold``. If there are
-        more distinct values than the ``distinct_threshold``, then distinct_overflow flag is set and
-        list of distinct values will be empty"""
+    def _probe_distinct(self, value):
+        """"""
         if self.distinct_overflow:
             return
 
@@ -75,7 +105,16 @@ class FieldStatistics(object):
         else:
             self.distinct_overflow = True
             
-    def finalize(self):
+    def finalize(self, record_count = None):
+        """Compute final statistics.
+        
+        :Parameters:
+            * `record_count`: final number of records in probed dataset.
+                See :meth:`FieldStatistics` for more information.
+        """
+        if record_count:
+            self.record_count = record_count
+
         if self.record_count:
             self.value_ratio = float(self.value_count) / float(self.record_count)
             self.null_record_ratio = float(self.null_count) / float(self.record_count)
@@ -87,6 +126,7 @@ class FieldStatistics(object):
             self.unique_storage_type = list(self.storage_types)[0]
 
     def dict(self):
+        """Return dictionary representation of receiver."""
         d = {}
         d["key"]= self.field
         d["value_count"]= self.value_count
