b57f2c0186e784d9b8928969aea10931a761eea2
Stefan Urbanek <stefan.urbanek@gmail.com>
2011-02-09 15:11:48 +0100

    added node network basic test

--- brewery/pipes/network.py
+++ brewery/pipes/network.py
@@ -65,3 +65,38 @@ class Network(object):
         target_node = self.node(target)
 
         self.connections.discard( (source_node, target_node) )
+
+
+    def _sort_nodes(self):
+        """
+        L ← Empty list that will contain the sorted elements
+        S ← Set of all nodes with no incoming edges
+        while S is non-empty do
+            remove a node n from S
+            insert n into L
+            for each node m with an edge e from n to m do
+                remove edge e from the graph
+                if m has no other incoming edges then
+                    insert m into S
+        if graph has edges then
+            output error message (graph has at least one cycle)
+        else 
+            output message (proposed topologically sorted order: L)
+        """
+        pass
+    def initialize(self):
+        """Initializes the data processing network:
+        
+        * sorts nodes based on connection dependencies
+        * creates pipes between nodes
+        * initializes each node
+        * initializes pipe fields
+        
+        """
+
+        self.pipes = []
+        
+        # self._sort_nodes()
+        # sorted_nodes = self.sort
+        
+        
