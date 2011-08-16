a4871d9dc1d00fefb241ff8723eeb28acb48a5d7
Stefan Urbanek <stefan.urbanek@gmail.com>
2011-08-16 11:43:47 +0200

    Preliminary implementation of data probes (just concept, API not decided yet for 100%)

--- brewery/probes.py
+++ brewery/probes.py
@@ -1,3 +1,39 @@
+"""Data probes"""
+
+import utils
+import re
+
+__all__ = [
+    "MissingValuesProbe",
+    "StatisticsProbe",
+    "DistinctProbe",
+    "StorageTypeProbe",
+    "MultiProbe",
+    "CompletenessProbe"
+]
+
+class MultiProbe(object):
+    """Probe with multiple probes"""
+    def __init__(self, probes = None):
+        if probes:
+            self.probes = probes
+        else:
+            self.probes = []
+        
+    def probe(self, value):
+        for probe in self.probes:
+            probe.probe(value)
+        
+
+    def to_dict(self):
+        d = {}
+        for probe in self.probes:
+            name = utils.to_identifier(utils.decamelize(probe.__class__.__name__))
+            re.sub('_probe$', name, '')
+            d[name] = probe.to_dict()
+            
+        return d
+
 class MissingValuesProbe(object):
     """Data quality statistics for a dataset field
     
@@ -14,16 +50,39 @@ class MissingValuesProbe(object):
         if value is None:
             self.count += 1
 
+    def to_dict(self):
+        return {"count": self.count}
+
+class CompletenessProbe(object):
+    """Data quality statistics for a dataset field
+
+    :Attributes:
+        * `count`: total count of records
+        * `unknown`: number of unknown records (NULL, None, nil, ...)
+    """    
+    def __init__(self):
+        self.count = 0
+        self.unknown = 0
+
+    def probe(self, value):
+        """Probe the value.
+        """
+        self.count += 1
+        if value is None:
+            self.unknown += 1
+            
+    def to_dict(self):
+        return {"count": self.count, "unknown": self.unknown}
+
 class StatisticsProbe(object):
     """Data quality statistics for a dataset field
 
     :Attributes:
-        * `record_count`: total count of records in dataset. This should be set explicitly on
-          finalisation. Seet :meth:`FieldStatistics.finalize`. In relational database this should be the
-          same as `value_count`.
-
-        * `fields`: 
-        * `count`: number of non-null objects
+        * `min` - minimum value found
+        * `max` - maxumum value found
+        * `sum` - sum of values
+        * `count` - count of values
+        * `average` - average value
     """    
     def __init__(self):
         self.min = None
@@ -46,7 +105,12 @@ class StatisticsProbe(object):
                 self.min = min(self.min, value)
                 self.max = max(self.max, value)
 
+    def to_dict(self):
+        return {"count": self.count, "min": self.min, "max": self.max,
+                "sum": self.sum, "average": self.average }
+
 class DistinctProbe(object):
+    """Probe for distinct values."""
     def __init__(self, threshold = None):
         self.distinct_values = set([])
         self.overflow = False
@@ -83,4 +147,10 @@ class StorageTypeProbe(object):
             return list(self.storage_types)[0]
         else:
             return None
-    
+
+    def to_dict(self):
+        d = {
+            "storage_types": [str(st) for st in self.storage_types],
+            "unique_storage_type": self.unique_storage_type
+        }
+        return d
