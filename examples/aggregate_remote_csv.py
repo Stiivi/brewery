265962afd7c55725f9f47b786b55a235251a6133
Stefan Urbanek <stefan@agentfarms.net>
2012-04-03 22:45:04 +0200

    added aggregation example

---
+++ examples/aggregate_remote_csv.py
@@ -0,0 +1,24 @@
+"""
+Data Brewery Example
+
+Aggregate a remote CSV file.
+"""
+import brewery
+
+main = brewery.create_builder()
+
+main.csv_source("https://raw.github.com/Stiivi/cubes/master/examples/hello_world/data.csv")
+main.node.fields = brewery.FieldList([
+                                "category_code",
+                                "category",
+                                "subcategory_code",
+                                "subcategory", 
+                                "line_item", 
+                                "year", 
+                                ["amount", "float"] 
+                            ])
+main.aggregate(keys=["year", "category"], measures=["amount"])
+main.field_map(keep_fields=["year", "category", "amount_sum"])
+main.formatted_printer()
+
+main.stream.run()
