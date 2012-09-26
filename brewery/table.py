147a465be3049994bc7a2d0958a322926fb8c1e7
Stefan Urbanek <stefan@agentfarms.net>
2012-09-26 00:08:18 -0500

    default dummy table implementation

---
+++ brewery/table.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+
+class PythonTable(object):
+    """Default table implementation with python objects."""
+    def __init__(self, columns, fields):
+        self.table = OrderedDict()
+        for field, column in zip(fields, columns):
+            self.table[str(field)] = column
+
+    def column(self, name):
+        return self.table[name]
+
+    def rows(self):
+        """Iterate through table rows."""
+        cols = self.table.values()
+        for row in zip(*cols):
+            yield row
+
+    def __iter__(self):
+        cols = self.table.values()
+        for row in zip(*cols):
+            yield row
+
+
