57f1a8503054145e46002c99444dedaea583e5fc
Stefan Urbanek <stefan@agentfarms.net>
2012-09-27 18:58:34 -0500

    new array based Stream - new way of initialization and run()

--- brewery/streams.py
+++ brewery/streams.py
@@ -1,23 +1,34 @@
-from table import PythonTable
+# -*- coding: utf-8 -*-
+
 from errors import *
 from graph import *
 from .utils import get_logger
 from .metadata import *
+from collections import namedtuple
+from .common import get_backend
 
 __all__ = [
         "Stream"
         ]
 
+# Sources - list of FieldLists
+# target - 
+NodeContext = namedtuple("NodeContext",
+                         ["node", "name", "sources", "target"])
+
 class Stream(Graph):
 
-    def __init__(self, nodes=None, connections=None):
+    def __init__(self, nodes=None, connections=None, backend=None):
         super(Stream, self).__init__(nodes, connections)
 
         # FIXME: use factory
         self.field_arrays = {}
         self.logger = get_logger()
 
-
+        if backend:
+            self.backend = get_backend(backend)
+        else:
+            self.backend = get_backend("default")
 
     def initialize(self, nodes):
         """Initializes the `nodes`. If any node fails in initialization, then
@@ -28,17 +39,17 @@ class Stream(Graph):
 
         for node in nodes:
             self.logger.debug("creating pipes for node %s" % node)
-
+            print "--> initializing node %s" % node
             sources = self.node_sources(node)
             fieldlists = [source.output_fields for source in sources]
             self.logger.debug("initializing node of type %s" % node.__class__)
-            node.initalize_fields(fieldlists)
+            node.initialize_fields(fieldlists)
             node.initialize()
             # FIXME: remove ^^
 
 			# raise exception with failed finalizations and failed initializations
 
-	def finalize(self, nodes):
+    def finalize(self, nodes):
 		failed = []
 		for node in nodes:
 			try:
@@ -82,37 +93,58 @@ class Stream(Graph):
             raise MetadataError("Field %s is not frozen" % field)
 
         array = self.field_arrays.get(origin)
-        if array:
-            return array
+        if not array:
+            array = self.backend.create_array(field)
+            self.field_arrays[field] = array
 
-        return self.create_array(field)
-
-    def create_array(self, field):
-        """Create an array for `field`."""
-
-        # FIXME: use array factory
-        array = list()
-        self.field_arrays[field] = array
         return array
 
-    def run(self):
-        nodes = self.sorted_nodes()
-        print "NODES: %s" % nodes
+    def run(self, nodes=None):
+        nodes = self.sorted_nodes(nodes)
+
         self.initialize(nodes)
 
+        # Create node wrappers
+        wrappers = []
+        tables = {}
+
         for node in nodes:
-            self.logger.info("Running node %s" % node)
+            print "=== wrapping node %s" % node
+            name = self.node_name(node)
 
+            sources = []
+            for inode in self.node_sources(node):
+                table = tables[inode]
+                if table is not None:
+                    sources.append(table)
+                else:
+                    raise Exception("No table for node %s (%d)" % (inode,
+                        len(tables)))
+            print "--- sources: %d" % len(sources)
             fields = node.output_fields
+            if fields:
+                print "--- constructing table with fields: %s" % (fields, )
+                arrays = [self.get_array(field) for field in fields]
+                table = self.backend.create_table(arrays, fields)
+            else:
+                print "--- no table (is target)"
+                table = None
+            # print "--- storing table '%s' for %s" % (repr(table), node)
+            tables[node] = table
 
-            arrays = [self.get_array(field) for field in fields]
-            table = self.create_table(arrays, fields)
+            wrappers.append(NodeContext(node,name,sources,table))
 
-            node.run(table)
+        for wrapper in wrappers:
+            print ("=== Running node %s (%s)" % (wrapper.name,
+                                                        wrapper.node))
+
+
+            wrapper.node.run(sources=wrapper.sources,target=wrapper.target)
+            # print "RESULT:"
+            # print "========================="
+            # print wrapper.target
+            # print "========================="
 
         self.finalize(nodes)
-
-    def create_table(self, arrays, fields):
-        return PythonTable(arrays, fields)
 
 
