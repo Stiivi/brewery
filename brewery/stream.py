7b30f9b9136d90bfd80ebf0b08badbdd5939a091
Stefan Urbanek <stefan@agentfarms.net>
2013-01-17 16:53:44 +0100

    Added execution context to the node evaluation call

--- brewery/stream.py
+++ brewery/stream.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from errors import *
-from graph import *
+from .errors import *
+from .graph import *
 from .common import get_backend, get_logger
 from .metadata import *
-from collections import namedtuple
 from .nodes.base import node_dictionary
-import logging
+from collections import namedtuple
 
 __all__ = [
         "Stream"
@@ -25,9 +24,6 @@ class Stream(Graph):
         # FIXME: use factory
         self.field_arrays = {}
         self.log = get_logger()
-
-        if __debug__:
-            self.log.setLevel(logging.DEBUG)
 
         if backend:
             self.backend = get_backend(backend)
@@ -66,37 +62,29 @@ class Stream(Graph):
 
         To fork a fork, just call ``fork()``
         """
-
-        return _StreamFork(self)
+        return StreamFork(self)
 
     def initialize(self, nodes):
-        """Initializes the `nodes`. If any node fails in initialization, then
-        finalization is called for each already initialized node and exception is
-        raised."""
-        # FIXME: check for repeating fields (each node should generate its own)
-        # FIXME: handle exception during node initialization (finalize)
+        """Initializes the `nodes`. Does nothing – obsolete.
+        """
+        pass
 
-        for node in nodes:
-            self.log.debug("initializing node %s" % node)
-            sources = self.node_sources(node)
-            fieldlists = [source.output_fields for source in sources]
-
-            node.initialize_fields(fieldlists)
 
     def finalize(self, nodes):
-		failed = []
-		for node in nodes:
-			try:
-				if hasattr(node, finalize):
-					node.finalize()
-			except Exception as e:
-				failed.append( (node, e) )
+        """Allows nodes to clean-up after execution or exception."""
+        failed = []
+        for node in nodes:
+            try:
+                if hasattr(node, finalize):
+                    node.finalize()
+            except Exception as e:
+                failed.append( (node, e) )
 		# FIXME: raise exception with list of all failed nodes
 
-    def last_compatible_origin(self, field):
+    def _last_compatible_origin(self, field):
         """Returns origin that is a `Field` of compatible storage type.
         Returns `field` if no other origin is found."""
-
+        # FIXME: currently unused, keeping here for future use
         visited = set()
         last_field = field
 
@@ -121,6 +109,7 @@ class Stream(Graph):
     def get_array(self, field):
         """Gets an array for `field`. If no array for field exists, then new
         one is created using array backend."""
+        # FIXME: currently unused, keeping here for future use
 
         origin = self.last_compatible_origin(field)
         if not origin:
@@ -144,16 +133,15 @@ class Stream(Graph):
         # prepare(inputs)
         # where inputs is list of (fields, representations)
 
-        nodes = self.sorted_nodes(nodes)
-
-        # csv.0 -> meh.1
-        # in meh, for 1
-        # key: 1
         node_outputs = {}
+        nodes = self.sorted_nodes(nodes)
+        # FIXME: use some context object instead
+        context = ExecutionContext()
+
         for node in nodes:
             self.logger.info("evaluating node %s" % node)
             conns = self.connections_with_target(node)
-            self.logger.info("    number of connections: %d" % len(conns))
+            self.logger.debug("    number of connections: %d" % len(conns))
             sources = {}
             for i, c in enumerate(conns):
                 source_key = i if c.source_outlet is None else c.source_outlet
@@ -165,75 +153,40 @@ class Stream(Graph):
                     raise BreweryError("Unknown outlet '%s' in node %s" %
                                             (key, c.source))
                 sources[target_key] = out
-            result = node.evaluate(sources)
+
+            result = node.evaluate(context, sources)
+
             if not isinstance(result, dict):
                 outputs = {0:result}
             else:
                 outputs = result
 
-            self.logger.info("result: %s" % (outputs, ))
+            self.logger.debug("result: %s" % (outputs, ))
             node_outputs[node] = outputs
 
-    def _run_arrayed(self, nodes=None):
-        """Runs the stream"""
+class ExecutionContext(object):
+    def __init__(self):
+        self.logger = get_logger()
+        self.actions = []
 
-        """Notes:
-            * run should take context as argument, where context is:
-                * backend used
-                * source tables
-        """
-        # FIXME: temporarily depreciated
-        raise Exception("Temporarily depreciated")
-        nodes = self.sorted_nodes(nodes)
+    def action(self, node, action, options):
+        self.actions.append( (node, action, options) )
 
-        self.initialize(nodes)
-
-        # Create node wrappers
-        wrappers = []
-        tables = {}
-
-        for node in nodes:
-            self.log.debug("wrapping node %s" % node)
-            name = self.node_name(node)
-
-            sources = []
-            for inode in self.node_sources(node):
-                table = tables[inode]
-                if table is not None:
-                    sources.append(table)
-                else:
-                    raise Exception("No table for node %s (%d)" % (inode,
-                        len(tables)))
-
-            fields = node.output_fields
-            if fields:
-                self.log.debug("constructing table with fields: %s" % (fields,))
-                arrays = [self.get_array(field) for field in fields]
-                table = self.backend.create_table(arrays, fields)
-            else:
-                self.log.debug("no table (is target)")
-                table = None
-            # print "--- storing table '%s' for %s" % (repr(table), node)
-            tables[node] = table
-
-            wrappers.append(NodeContext(node,name,sources,table))
-        self.log.debug("total arrays created: %d" % len(self.field_arrays))
-        self.log.info("running %d nodes" % len(wrappers))
-        for wrapper in wrappers:
-            self.log.info("running node %s (%s)" % (wrapper.name,
-                                                        wrapper.node.identifier()))
+    def error(self, *args, **kwargs):
+        self.logger.error(*args, **kwargs)
+    def warn(self, *args, **kwargs):
+        self.logger.warn(*args, **kwargs)
+    def info(self, *args, **kwargs):
+        self.logger.info(*args, **kwargs)
+    def debug(self, *args, **kwargs):
+        self.logger.debug(*args, **kwargs)
 
 
-            wrapper.node.run(sources=wrapper.sources,target=wrapper.target)
-        self.log.info("finished running")
-        self.finalize(nodes)
-
-
-class _StreamFork(object):
+class StreamFork(object):
     """docstring for StreamFork"""
     def __init__(self, stream, node=None):
         """Creates a stream fork - class for building streams."""
-        super(_StreamFork, self).__init__()
+        super(StreamFork, self).__init__()
         self.stream = stream
         self.node = node
 
@@ -248,6 +201,24 @@ class _StreamFork(object):
 
         return self
 
+    def source(self, obj):
+        """Create a generic source node with data object `obj`. Assusres
+        source version of the object by calling `as_source()`"""
+        # TODO: if there is specific node class that can wrap the source, then
+        # use it
+        obj = obj.as_source()
+        self += DataObjectSource(obj)
+        return self
+
+    def target(self, store, name, **ops):
+        """Create a generic target node with data object `obj`. Assures target
+        version of the object by calling `as_target()`"""
+        # TODO: if there is specific node class that can wrap the target, then
+        # use it
+        obj = obj.as_target()
+        self += DataObjectTarget(obj)
+        return self
+
     def set_name(self, name):
         """Sets name of current node."""
         self.stream.set_node_name(self.node, name)
@@ -255,7 +226,7 @@ class _StreamFork(object):
     def fork(self):
         """Forks current fork. Returns a new fork with same actual node as the fork being
         forked."""
-        fork = _StreamFork(self.stream, self.node)
+        fork = StreamFork(self.stream, self.node)
         return fork
 
     def merge(self, obj, **kwargs):
@@ -278,6 +249,10 @@ class _StreamFork(object):
         # self.stream.append(merge)
         # self.stream.connect()
 
+    def transform(self):
+        """Provides transformation context. """
+        return TransformationBuilder(self, self.node)
+
     def append(self, obj):
         """Appends data from nodes using AppendNode"""
         raise NotImplementedError
@@ -285,15 +260,17 @@ class _StreamFork(object):
     def __getattr__(self, name):
         """Returns node class"""
         # FIXME: use create_node here
-
         class_dict = node_dictionary()
 
-        node_class = class_dict[name]
+        try:
+            node_class = class_dict[name]
+        except KeyError:
+            raise StreamError("Node of type %s does not exist" % name)
 
-        constructor = _StreamForkConstructor(self, node_class)
-        return constructor
+        builder = StreamForkBuilder(self, node_class)
+        return builder
 
-class _StreamForkConstructor(object):
+class StreamForkBuilder(object):
     """Helper class to append new node."""
     def __init__(self, fork, node_class):
         self.fork = fork
@@ -304,3 +281,19 @@ class _StreamForkConstructor(object):
         self.fork += node
         return self.fork
 
+
+class TransformationContext(object):
+    def __init__(self, fork, node, fields):
+        self.node = node
+        self.fork = fork
+        self.fields = fields
+
+    def __enter__(self):
+        return (TransformationTarget(context, fields),
+                TransformationSource(context, fields))
+
+    def __exit__(exc_type, exc_val, exc_tb):
+        if exc_type:
+            return False
+        else:
+            return True
